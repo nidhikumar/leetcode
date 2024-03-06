cb688aab444057b84738c0ddd53cadc0d077835e
Nidhi Kumar <nidhikr1401@gmail.com>
2024-03-06 15:34:09 -0800

    fibonacci dp

---
+++ fibonacci.py
@@ -0,0 +1,15 @@
+def fib(n,dp):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    elif dp[n] != -1:
+        return dp[n]
+    else:
+        dp[n] = fib(n-1,dp) + fib(n-2, dp)
+        return dp[n]
+
+dp = [-1] * 100
+print(fib(4,dp))
+
+# 0,1,1,2,3,
