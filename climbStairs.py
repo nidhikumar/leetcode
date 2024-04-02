8b00157ae4846d0c1e91d4c9778beaa38aa8688b
Nidhi Kumar <nidhikr1401@gmail.com>
2024-04-02 14:09:08 -0700

    climbing stairs

---
+++ climbStairs.py
@@ -0,0 +1,11 @@
+def climbStairs(n):
+    one, two = 1, 1
+    for i in range(n-1):
+        temp = one
+        one = one + two
+        two = temp
+    return one
+
+print(climbStairs(4))
+
+# 0,1,1,2,3
